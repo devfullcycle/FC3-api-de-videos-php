903b4d172b368d322ac0b304cfc2148e8d91678f
Carlos Ferreira <carlos@especializati.com.br>
2023-04-21 15:01:30 -0300

    feat: tdd - test property createdat entity

--- src/Core/Category/Domain/Entities/Category.php
+++ src/Core/Category/Domain/Entities/Category.php
@@ -2,18 +2,26 @@
 
 namespace Core\Category\Domain\Entities;
 
+use DateTime;
+
 class Category
 {
     public function __construct(
         protected string $name,
         protected ?string $description = null,
         protected bool $isActive = true,
+        protected ?DateTime $createdAt = null,
     ) {
-        // 
+        $this->createdAt = $this->createdAt ?? new DateTime();
     }
 
     public function __get($name)
     {
         return $this->{$name};
+    }
+
+    public function createdAt(): string
+    {
+        return (string) $this->createdAt->format('Y-m-d H:i:s');
     }
 }
--- tests/Unit/Core/Category/Domain/Entities/CategoryUnitTest.php
+++ tests/Unit/Core/Category/Domain/Entities/CategoryUnitTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use DateTime;
 use Core\Category\Domain\Entities\Category;
 
 beforeEach(fn () => $this->category = new Category(
@@ -12,14 +13,22 @@ test('constructor of category and getters and setters', function () {
     expect($this->category->name)->toBe('test');
     expect($this->category->description)->toBeNull();
     expect($this->category->isActive)->toBe(true);
+    expect($this->category->createdAt)->not->toBeNull();
+    expect($this->category->createdAt)->toBeInstanceOf(DateTime::class);
 
+    $date = new DateTime();
     $category = new Category(
         name: 'test',
         description: 'desc',
-        isActive: false
+        isActive: false,
+        createdAt: $date,
     );
     expect($category->description)->not->toBeNull();
     expect($category->description)->toBe('desc');
     expect($category->isActive)->toBeFalsy();
     expect($category->isActive)->toBeBool();
+    expect($category->createdAt)->not->toBeNull();
+    expect($category->createdAt)->toBe($date);
+    expect($category->createdAt())->toBeString();
+    expect($category->createdAt())->not->toBeNull();
 });
