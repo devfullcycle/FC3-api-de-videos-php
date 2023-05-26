3553389f3df123dafb25e5b7efdb478154b6f3f8
Carlos Ferreira <carlos@especializati.com.br>
2023-05-26 16:23:45 -0300

    docs: add doc type of array

--- src/Core/Category/Application/DTO/OutputCategoriesDTO.php
+++ src/Core/Category/Application/DTO/OutputCategoriesDTO.php
@@ -5,6 +5,9 @@ namespace Core\Category\Application\DTO;
 class OutputCategoriesDTO
 {
     public function __construct(
+        /**
+         * @var array<OutputCategoryDTO>
+         */
         public readonly array $items,
         public readonly int $total,
     ) {}
