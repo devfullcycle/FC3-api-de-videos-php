a2e6d3e19fe7309c3c2d75578b72e3b43924d596
Carlos Ferreira <carlos@especializati.com.br>
2023-06-30 18:00:27 +0000

    test: get one category from elasticsearch

--- tests/Unit/Core/Category/Infra/CategoryRepositoryUnitTest.php
+++ tests/Unit/Core/Category/Infra/CategoryRepositoryUnitTest.php
@@ -1,7 +1,9 @@
 <?php
 
+use Core\Category\Domain\Entities\Category;
 use Core\Category\Infra\CategoryRepository;
 use Core\Category\Infra\Contracts\ElasticClientInterface;
+use Core\SeedWork\Domain\Exceptions\EntityNotFoundException;
 
 test('findAll', function () {
     $mockElastic = Mockery::mock(ElasticClientInterface::class);
@@ -26,4 +28,60 @@ test('findAll', function () {
     $categoryRepository = new CategoryRepository($mockElastic);
     $response = $categoryRepository->findAll('test_name');
     expect($response)->toBeArray();
+});
+
+test('findOne: should exception when total register returned is equals zero', function () {
+    $mockElastic = Mockery::mock(ElasticClientInterface::class);
+    $mockElastic->shouldReceive('search')
+        ->times(1)
+        ->andReturn([
+            'hits' => [
+                'hits' => []
+            ]
+        ]);
+
+    $categoryRepository = new CategoryRepository($mockElastic);
+    $categoryRepository->findOne('fake_value');
+})->throws(EntityNotFoundException::class);
+
+test('findOne', function () {
+    $id = '88bdf4aa-7ec7-408f-91f6-c82f192d540c';
+
+    $mockElastic = Mockery::mock(ElasticClientInterface::class);
+    $mockElastic->shouldReceive('search')
+        ->times(1)
+        ->with([
+            'index' => 'mysql-server.fullcycle.categories',
+            'body' => [
+                'query' => [
+                    'match' => [
+                        'after.id' => $id
+                    ]
+                ]
+            ]
+        ])
+        ->andReturn([
+            'hits' => [
+                'hits' => [
+                    [
+                        '_source' => [
+                            'after' => [
+                                'id' => $id,
+                                'name' => 'Cat Name',
+                                'description' => 'Desc',
+                                'is_active' => 1,
+                                'created_at' => '2023-12-12 12:12:00',
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+
+    $categoryRepository = new CategoryRepository($mockElastic);
+    $category = $categoryRepository->findOne($id);
+    expect($category)->toBeInstanceOf(Category::class);
+    expect($category->name)->toBe('Cat Name');
+    expect($category->description)->toBe('Desc');
+    expect($category->isActive)->toBeTrue();
 });
