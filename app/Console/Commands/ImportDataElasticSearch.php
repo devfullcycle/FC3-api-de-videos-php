1b434401e4237c1ecbc542f1b850e81916436f35
Carlos Ferreira <carlos@especializati.com.br>
2023-07-15 17:59:44 +0000

    feat: cli import data to elasticsearch

---
+++ app/Console/Commands/ImportDataElasticSearch.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Core\Category\Infra\Contracts\ElasticClientInterface;
+use Illuminate\Console\Command;
+
+class ImportDataElasticSearch extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    // php artisan app:import-data-elastic-search mysql-server.fullcycle.categories
+    protected $signature = 'app:import-data-elastic-search {index}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import data elastic search';
+
+    public function __construct(protected ElasticClientInterface $elasticClient)
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $index = $this->argument('index');
+
+        $fileContents = file(base_path("tests/dumps/import-{$index}.csv"));
+        foreach ($fileContents as $line) {
+            if ($line === "\n") continue;
+            // Split the string into an array based on the delimiter '&'
+            $parts = explode(',', $line);
+
+            // Create the key-value pairs from the array
+            $content = [];
+            foreach ($parts as $part) {
+                // Split each part into key and value based on the delimiter ':'
+                $pair = explode(':', $part);
+                $key = $pair[0];
+                $value = str_replace('\n', '', trim($pair[1]));
+                $content[$key] = $value;
+            }
+            $this->elasticClient->createIndex(
+                'import-' . $index,
+                ['after' => $content]
+            );
+        }
+    }
+}
