214ac5b26817319c8cbc556f27c25e22d36cc8a7
Carlos Ferreira <carlos@especializati.com.br>
2023-08-05 12:57:37 +0000

    test: e2e get single category

--- tests/Feature/Api/CategoryApiTest.php
+++ tests/Feature/Api/CategoryApiTest.php
@@ -17,3 +17,24 @@ test('category api e2e: list all categories', function () {
             ]
         ]);
 });
+
+test('category api e2e: list single category', function () {
+    $response = $this->getJson('/categories/07167628-33b7-4584-8444-d1dc7ed436a9');
+
+    $response->assertOk()
+        ->assertJsonStructure([
+            'data' => [
+                'id',
+                'name',
+                'description',
+                'is_active',
+                'created_at',
+            ]
+        ]);
+
+    expect($response['data']['id'])->toBe('07167628-33b7-4584-8444-d1dc7ed436a9');
+    expect($response['data']['name'])->toBe('Daniella Kunde');
+    expect($response['data']['description'])->toBe('Consectetur sit iusto qui distinctio molestiae vitae nemo qui.');
+    expect($response['data']['is_active'])->toBeTrue();
+    expect($response['data']['created_at'])->toBe('2023-06-22 14:26:57');
+});
