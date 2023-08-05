37bb7e7ba3b9131cac5910b2c21836a2bf8c1fe1
Carlos Ferreira <carlos@especializati.com.br>
2023-08-05 13:41:12 +0000

    test: using datasets in e2e graphql

--- tests/Feature/GraphQL/CategoryGraphQLTest.php
+++ tests/Feature/GraphQL/CategoryGraphQLTest.php
@@ -1,27 +1,25 @@
 <?php
 
-test('test e2e graphql: list all categories', function () {
+test('test e2e graphql: list all categories', function (string $valuesRequest, array $valuesExpected) {
     $response = $this->post('/graphql', [
-        'query' => '{
+        'query' => "{
             categories {
-                id
-                name
-                description
+                $valuesRequest
             }
-        }'
+        }"
     ]);
 
     $response->assertStatus(200);
     $response->assertJsonCount(10, 'data.categories');
-    $response->assertJsonStructure([
-        'data' => [
-            'categories' => [
-                '*' => [
-                    'id',
-                    'name',
-                    'description',
-                ]
-            ]
-        ]
-    ]);
-});
+    array_map(
+        fn ($category) => expect(array_keys($category))->toBe($valuesExpected),
+        $response['data']['categories']
+    );
+})->with([
+    'case 01' => ['id name', ['id', 'name']],
+    'case 02' => ['id name is_active', ['id', 'name', 'is_active']],
+    'case 03' => ['id', ['id']],
+    'case 04' => ['id description', ['id', 'description']],
+    'case 05' => ['id name description', ['id', 'name', 'description']],
+    'case 06' => ['id name description is_active created_at', ['id', 'name', 'description', 'is_active', 'created_at']],
+]);
